43b41df2e8677443fbaf692ae2dfb7f8d6b3aae6
gael <gael@imotionfactory.com>
2013-09-26 12:52:55 +0200

    Issue #0100: disabled warnings and errors

--- BeshakLanding/public/index.php
+++ BeshakLanding/public/index.php
@@ -1,6 +1,6 @@
 <?php
-	error_reporting(E_ALL);
-	ini_set('display_errors', '1');
+// 	error_reporting(E_ALL);
+// 	ini_set('display_errors', '1');
 	$email = $_POST["email"];
 	if (isset($email)) {
 		$appPath = $_SERVER['DOCUMENT_ROOT'].'/../application/';
